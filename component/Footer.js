77ff0a58016256b867b0350c3fa99fd4b49da4e5
Adedayo <owoyeleadedayo25@gmail.com>
2022-08-03 13:37:35 +0100

    initial

---
+++ component/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+     return ( 
+          <footer>
+               Copyright 2022 ODB MADE IT WITH LOVE
+          </footer>
+      );
+}
+ 
+export default Footer;
---
+++ pages/about.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+const about = () => {
+     return ( 
+          <>
+               <Head>
+                    <title> Next List | About </title>
+                    <meta name='keywords' content='next' />
+               </Head>
+               <div>
+                    <h1>You don't know me Nigga</h1>
+                    <p>ReactJS is a declarative, efficient, and flexible JavaScript library for building reusable UI components. It is an open-source, component-based front end library which is responsible only for the view layer of the application. It was initially developed and maintained by Facebook and later used in its products like WhatsApp & Instagram.</p>
+               </div>
+          </>
+      );
+}
+ 
+export default about;
